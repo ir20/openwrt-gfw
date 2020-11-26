c022b7f66c997fd2538c1e42f1ddc2639c7c9608
ir20 <60230695+ir20@users.noreply.github.com>
2020-11-26 07:02:29 +0000

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,5 +1,4 @@
 import scrapy
-import re
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
@@ -8,7 +7,7 @@ class UrlSpider(CrawlSpider):
     start_urls = ["https://downloads.openwrt.org/releases/19.07.4/targets/"]
 
     rules = (
-        Rule(LinkExtractor(allow=("https://downloads.openwrt.org/releases/19.07.4/targets/.*/openwrt-sdk")), callback="parse_item", process_request="method_head"),
+        Rule(LinkExtractor(allow=("https://downloads.openwrt.org/releases/19.07.4/targets/.*/openwrt-sdk"), deny_extensions=()), callback="parse_item", process_request="method_head"),
         Rule(LinkExtractor(allow=("https://downloads.openwrt.org/releases/19.07.4/targets/.*/$"), deny=("kmods/$", "packages/$")))
     )
 
